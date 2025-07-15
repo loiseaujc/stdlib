0d890801004a8adb01c9d09d73166f77f87edc9e
loiseaujc <loiseau.jc@gmail.com>
2025-07-15 09:18:35 +0000

    Deploying to stdlib-fpm from @ loiseaujc/stdlib@ed0a4e0942d175b552d2aded11ab8c61400e6350 🚀

--- test/test_linalg_expm.f90
+++ test/test_linalg_expm.f90
@@ -1,6 +1,8 @@
-! Test Schur decomposition
+
+! Test the matrix exponential.
 module test_linalg_expm
     use testdrive, only: error_type, check, new_unittest, unittest_type
+    use stdlib_constants
     use stdlib_linalg_constants
     use stdlib_linalg, only: expm, eye, norm, matrix_exp
     use stdlib_linalg_state, only: linalg_state_type, linalg_error_handling, LINALG_ERROR, &
@@ -12,315 +14,290 @@ module test_linalg_expm
 
     contains
 
-    !> schur decomposition tests
+    ! gcc-15 bugfix utility
+    subroutine add_test(tests,new_test)
+        type(unittest_type), allocatable, intent(inout) :: tests(:)    
+        type(unittest_type), intent(in) :: new_test
+
+        integer :: n
+        type(unittest_type), allocatable :: new_tests(:)
+
+        if (allocated(tests)) then 
+            n = size(tests)
+        else
+            n = 0
+        end if
+
+        allocate(new_tests(n+1))
+        if (n>0) new_tests(1:n) = tests(1:n)
+                 new_tests(1+n) = new_test
+        call move_alloc(from=new_tests,to=tests)        
+
+    end subroutine add_test
+
+    !> Exponent of matrix tests
     subroutine test_expm_computation(tests)
         !> Collection of tests
         type(unittest_type), allocatable, intent(out) :: tests(:)
         
-        allocate(tests(0))
-        
-        tests = [tests, new_unittest("expm_s",test_expm_s)]
-        tests = [tests, new_unittest("Error-handling expm_s",test_error_handling_expm_s)]
-        tests = [tests, new_unittest("expm_d",test_expm_d)]
-        tests = [tests, new_unittest("Error-handling expm_d",test_error_handling_expm_d)]
-        tests = [tests, new_unittest("expm_c",test_expm_c)]
-        tests = [tests, new_unittest("Error-handling expm_c",test_error_handling_expm_c)]
-        tests = [tests, new_unittest("expm_z",test_expm_z)]
-        tests = [tests, new_unittest("Error-handling expm_z",test_error_handling_expm_z)]
+        call add_test(tests,new_unittest("expm",test_expm))
+        call add_test(tests,new_unittest("error_handling_expm",test_error_handling_expm))
 
     end subroutine test_expm_computation
 
     !> Matrix exponential with analytic expression.
-    subroutine test_expm_s(error)
+    subroutine test_expm(error)
         type(error_type), allocatable, intent(out) :: error
         ! Problem dimension.
         integer(ilp), parameter :: n = 5, m = 6
         ! Test matrix.
-        real(sp) :: A(n, n), E(n, n), Eref(n, n)
-        real(sp) :: err
         integer(ilp) :: i, j
 
-        ! Initialize matrix.
-        A = 0.0_sp
-        do i = 1, n-1
-            A(i, i+1) = m*1.0_sp
-        enddo
-
-        ! Reference with analytical exponential.
-        Eref = eye(n, mold=1.0_sp)
-        do i = 1, n-1
-            do j = 1, n-i
-                Eref(i, i+j) = Eref(i, i+j-1)*m/j
+        block
+            real(sp) :: A(n, n), E(n, n), Eref(n, n)
+            real(sp) :: err
+            
+            ! Initialize matrix.
+            A = zero_sp
+            do i = 1, n-1
+                A(i, i+1) = m*one_sp
             enddo
-        enddo
 
-        ! Compute matrix exponential.
-        E = expm(A)
-
-        ! Check result.
-        err = norm(Eref - E, "inf")
-        call check(error, err < (n**2)*epsilon(1.0_sp), "Analytical matrix exponential.")
-        if (allocated(error)) return
-        return        
-    end subroutine test_expm_s    
-    subroutine test_expm_d(error)
-        type(error_type), allocatable, intent(out) :: error
-        ! Problem dimension.
-        integer(ilp), parameter :: n = 5, m = 6
-        ! Test matrix.
-        real(dp) :: A(n, n), E(n, n), Eref(n, n)
-        real(dp) :: err
-        integer(ilp) :: i, j
-
-        ! Initialize matrix.
-        A = 0.0_dp
-        do i = 1, n-1
-            A(i, i+1) = m*1.0_dp
-        enddo
-
-        ! Reference with analytical exponential.
-        Eref = eye(n, mold=1.0_dp)
-        do i = 1, n-1
-            do j = 1, n-i
-                Eref(i, i+j) = Eref(i, i+j-1)*m/j
+            ! Reference with analytical exponential
+            Eref = eye(n, mold=one_sp)
+            do i = 1, n-1
+                do j = 1, n-i
+                    Eref(i, i+j) = Eref(i, i+j-1)*m/j
+                enddo
             enddo
-        enddo
 
-        ! Compute matrix exponential.
-        E = expm(A)
+            ! Compute matrix exponential.
+            E = expm(A)
 
-        ! Check result.
-        err = norm(Eref - E, "inf")
-        call check(error, err < (n**2)*epsilon(1.0_dp), "Analytical matrix exponential.")
-        if (allocated(error)) return
-        return        
-    end subroutine test_expm_d    
-    subroutine test_expm_c(error)
-        type(error_type), allocatable, intent(out) :: error
-        ! Problem dimension.
-        integer(ilp), parameter :: n = 5, m = 6
-        ! Test matrix.
-        complex(sp) :: A(n, n), E(n, n), Eref(n, n)
-        real(sp) :: err
-        integer(ilp) :: i, j
-
-        ! Initialize matrix.
-        A = 0.0_sp
-        do i = 1, n-1
-            A(i, i+1) = m*1.0_sp
-        enddo
-
-        ! Reference with analytical exponential.
-        Eref = eye(n, mold=1.0_sp)
-        do i = 1, n-1
-            do j = 1, n-i
-                Eref(i, i+j) = Eref(i, i+j-1)*m/j
+            ! Check result.
+            err = norm(Eref - E, "inf")
+            print *, err , (n**2)*epsilon(1.0_sp)
+            call check(error, err < (n**2)*epsilon(1.0_sp), "Analytical matrix exponential.")
+            if (allocated(error)) return
+        end block     
+        block
+            real(dp) :: A(n, n), E(n, n), Eref(n, n)
+            real(dp) :: err
+            
+            ! Initialize matrix.
+            A = zero_dp
+            do i = 1, n-1
+                A(i, i+1) = m*one_dp
             enddo
-        enddo
 
-        ! Compute matrix exponential.
-        E = expm(A)
-
-        ! Check result.
-        err = norm(Eref - E, "inf")
-        call check(error, err < (n**2)*epsilon(1.0_sp), "Analytical matrix exponential.")
-        if (allocated(error)) return
-        return        
-    end subroutine test_expm_c    
-    subroutine test_expm_z(error)
-        type(error_type), allocatable, intent(out) :: error
-        ! Problem dimension.
-        integer(ilp), parameter :: n = 5, m = 6
-        ! Test matrix.
-        complex(dp) :: A(n, n), E(n, n), Eref(n, n)
-        real(dp) :: err
-        integer(ilp) :: i, j
-
-        ! Initialize matrix.
-        A = 0.0_dp
-        do i = 1, n-1
-            A(i, i+1) = m*1.0_dp
-        enddo
-
-        ! Reference with analytical exponential.
-        Eref = eye(n, mold=1.0_dp)
-        do i = 1, n-1
-            do j = 1, n-i
-                Eref(i, i+j) = Eref(i, i+j-1)*m/j
+            ! Reference with analytical exponential
+            Eref = eye(n, mold=one_dp)
+            do i = 1, n-1
+                do j = 1, n-i
+                    Eref(i, i+j) = Eref(i, i+j-1)*m/j
+                enddo
             enddo
-        enddo
 
-        ! Compute matrix exponential.
-        E = expm(A)
+            ! Compute matrix exponential.
+            E = expm(A)
 
-        ! Check result.
-        err = norm(Eref - E, "inf")
-        call check(error, err < (n**2)*epsilon(1.0_dp), "Analytical matrix exponential.")
-        if (allocated(error)) return
-        return        
-    end subroutine test_expm_z    
+            ! Check result.
+            err = norm(Eref - E, "inf")
+            print *, err , (n**2)*epsilon(1.0_dp)
+            call check(error, err < (n**2)*epsilon(1.0_dp), "Analytical matrix exponential.")
+            if (allocated(error)) return
+        end block     
+        block
+            complex(sp) :: A(n, n), E(n, n), Eref(n, n)
+            real(sp) :: err
+            
+            ! Initialize matrix.
+            A = zero_sp
+            do i = 1, n-1
+                A(i, i+1) = m*one_sp
+            enddo
+
+            ! Reference with analytical exponential
+            Eref = eye(n, mold=one_sp)
+            do i = 1, n-1
+                do j = 1, n-i
+                    Eref(i, i+j) = Eref(i, i+j-1)*m/j
+                enddo
+            enddo
+
+            ! Compute matrix exponential.
+            E = expm(A)
+
+            ! Check result.
+            err = norm(Eref - E, "inf")
+            print *, err , (n**2)*epsilon(1.0_sp)
+            call check(error, err < (n**2)*epsilon(1.0_sp), "Analytical matrix exponential.")
+            if (allocated(error)) return
+        end block     
+        block
+            complex(dp) :: A(n, n), E(n, n), Eref(n, n)
+            real(dp) :: err
+            
+            ! Initialize matrix.
+            A = zero_dp
+            do i = 1, n-1
+                A(i, i+1) = m*one_dp
+            enddo
+
+            ! Reference with analytical exponential
+            Eref = eye(n, mold=one_dp)
+            do i = 1, n-1
+                do j = 1, n-i
+                    Eref(i, i+j) = Eref(i, i+j-1)*m/j
+                enddo
+            enddo
+
+            ! Compute matrix exponential.
+            E = expm(A)
+
+            ! Check result.
+            err = norm(Eref - E, "inf")
+            print *, err , (n**2)*epsilon(1.0_dp)
+            call check(error, err < (n**2)*epsilon(1.0_dp), "Analytical matrix exponential.")
+            if (allocated(error)) return
+        end block     
+    end subroutine test_expm
 
     !> Test error handler.
-    subroutine test_error_handling_expm_s(error)
+    subroutine test_error_handling_expm(error)
         type(error_type), allocatable, intent(out) :: error
         ! Problem dimension.
         integer(ilp), parameter :: n = 5, m = 6
         ! Test matrix.
-        real(sp) :: A(n, n), E(n, n)
+        
         type(linalg_state_type) :: err
         integer(ilp) :: i
 
-        ! Initialize matrix.
-        A = 0.0_sp
-        do i = 1, n-1
-            A(i, i+1) = m*1.0_sp
-        enddo
+        block
+            real(sp) :: A(n, n), E(n, n)
+            ! Initialize matrix.
+            A = zero_sp
+            do i = 1, n-1
+                A(i, i+1) = m*one_sp
+            enddo
 
-        ! Compute matrix exponential.
-        call matrix_exp(A, E, order=-1, err=err)
-        ! Check result.
-        call check(error, err%error(), "Negative Pade order")
-        if (allocated(error)) return
+            ! Compute matrix exponential.
+            call matrix_exp(A, E, order=-1, err=err)
+            ! Check result.
+            call check(error, err%error(), "Negative Pade order")
+            if (allocated(error)) return
 
-        call matrix_exp(A, order=-1, err=err)
-        ! Check result.
-        call check(error, err%error(), "Negative Pade order")
-        if (allocated(error)) return
+            call matrix_exp(A, order=-1, err=err)
+            ! Check result.
+            call check(error, err%error(), "Negative Pade order")
+            if (allocated(error)) return
 
-        ! Compute matrix exponential.
-        call matrix_exp(A, E(:n, :n-1), err=err)
-        ! Check result.
-        call check(error, err%error(), "Invalid matrix size")
-        if (allocated(error)) return
+            ! Compute matrix exponential.
+            call matrix_exp(A, E(:n, :n-1), err=err)
+            ! Check result.
+            call check(error, err%error(), "Invalid matrix size")
+            if (allocated(error)) return
 
-        ! Compute matrix exponential.
-        call matrix_exp(A(:n, :n-1), err=err)
-        ! Check result.
-        call check(error, err%error(), "Invalid matrix size")
-        if (allocated(error)) return
+            ! Compute matrix exponential.
+            call matrix_exp(A(:n, :n-1), err=err)
+            ! Check result.
+            call check(error, err%error(), "Invalid matrix size")
+            if (allocated(error)) return
+        end block
+        block
+            real(dp) :: A(n, n), E(n, n)
+            ! Initialize matrix.
+            A = zero_dp
+            do i = 1, n-1
+                A(i, i+1) = m*one_dp
+            enddo
 
-        return        
-    end subroutine test_error_handling_expm_s    
-    subroutine test_error_handling_expm_d(error)
-        type(error_type), allocatable, intent(out) :: error
-        ! Problem dimension.
-        integer(ilp), parameter :: n = 5, m = 6
-        ! Test matrix.
-        real(dp) :: A(n, n), E(n, n)
-        type(linalg_state_type) :: err
-        integer(ilp) :: i
+            ! Compute matrix exponential.
+            call matrix_exp(A, E, order=-1, err=err)
+            ! Check result.
+            call check(error, err%error(), "Negative Pade order")
+            if (allocated(error)) return
 
-        ! Initialize matrix.
-        A = 0.0_dp
-        do i = 1, n-1
-            A(i, i+1) = m*1.0_dp
-        enddo
+            call matrix_exp(A, order=-1, err=err)
+            ! Check result.
+            call check(error, err%error(), "Negative Pade order")
+            if (allocated(error)) return
 
-        ! Compute matrix exponential.
-        call matrix_exp(A, E, order=-1, err=err)
-        ! Check result.
-        call check(error, err%error(), "Negative Pade order")
-        if (allocated(error)) return
+            ! Compute matrix exponential.
+            call matrix_exp(A, E(:n, :n-1), err=err)
+            ! Check result.
+            call check(error, err%error(), "Invalid matrix size")
+            if (allocated(error)) return
 
-        call matrix_exp(A, order=-1, err=err)
-        ! Check result.
-        call check(error, err%error(), "Negative Pade order")
-        if (allocated(error)) return
+            ! Compute matrix exponential.
+            call matrix_exp(A(:n, :n-1), err=err)
+            ! Check result.
+            call check(error, err%error(), "Invalid matrix size")
+            if (allocated(error)) return
+        end block
+        block
+            complex(sp) :: A(n, n), E(n, n)
+            ! Initialize matrix.
+            A = zero_sp
+            do i = 1, n-1
+                A(i, i+1) = m*one_sp
+            enddo
 
-        ! Compute matrix exponential.
-        call matrix_exp(A, E(:n, :n-1), err=err)
-        ! Check result.
-        call check(error, err%error(), "Invalid matrix size")
-        if (allocated(error)) return
+            ! Compute matrix exponential.
+            call matrix_exp(A, E, order=-1, err=err)
+            ! Check result.
+            call check(error, err%error(), "Negative Pade order")
+            if (allocated(error)) return
 
-        ! Compute matrix exponential.
-        call matrix_exp(A(:n, :n-1), err=err)
-        ! Check result.
-        call check(error, err%error(), "Invalid matrix size")
-        if (allocated(error)) return
+            call matrix_exp(A, order=-1, err=err)
+            ! Check result.
+            call check(error, err%error(), "Negative Pade order")
+            if (allocated(error)) return
 
-        return        
-    end subroutine test_error_handling_expm_d    
-    subroutine test_error_handling_expm_c(error)
-        type(error_type), allocatable, intent(out) :: error
-        ! Problem dimension.
-        integer(ilp), parameter :: n = 5, m = 6
-        ! Test matrix.
-        complex(sp) :: A(n, n), E(n, n)
-        type(linalg_state_type) :: err
-        integer(ilp) :: i
+            ! Compute matrix exponential.
+            call matrix_exp(A, E(:n, :n-1), err=err)
+            ! Check result.
+            call check(error, err%error(), "Invalid matrix size")
+            if (allocated(error)) return
 
-        ! Initialize matrix.
-        A = 0.0_sp
-        do i = 1, n-1
-            A(i, i+1) = m*1.0_sp
-        enddo
+            ! Compute matrix exponential.
+            call matrix_exp(A(:n, :n-1), err=err)
+            ! Check result.
+            call check(error, err%error(), "Invalid matrix size")
+            if (allocated(error)) return
+        end block
+        block
+            complex(dp) :: A(n, n), E(n, n)
+            ! Initialize matrix.
+            A = zero_dp
+            do i = 1, n-1
+                A(i, i+1) = m*one_dp
+            enddo
 
-        ! Compute matrix exponential.
-        call matrix_exp(A, E, order=-1, err=err)
-        ! Check result.
-        call check(error, err%error(), "Negative Pade order")
-        if (allocated(error)) return
+            ! Compute matrix exponential.
+            call matrix_exp(A, E, order=-1, err=err)
+            ! Check result.
+            call check(error, err%error(), "Negative Pade order")
+            if (allocated(error)) return
 
-        call matrix_exp(A, order=-1, err=err)
-        ! Check result.
-        call check(error, err%error(), "Negative Pade order")
-        if (allocated(error)) return
+            call matrix_exp(A, order=-1, err=err)
+            ! Check result.
+            call check(error, err%error(), "Negative Pade order")
+            if (allocated(error)) return
 
-        ! Compute matrix exponential.
-        call matrix_exp(A, E(:n, :n-1), err=err)
-        ! Check result.
-        call check(error, err%error(), "Invalid matrix size")
-        if (allocated(error)) return
+            ! Compute matrix exponential.
+            call matrix_exp(A, E(:n, :n-1), err=err)
+            ! Check result.
+            call check(error, err%error(), "Invalid matrix size")
+            if (allocated(error)) return
 
-        ! Compute matrix exponential.
-        call matrix_exp(A(:n, :n-1), err=err)
-        ! Check result.
-        call check(error, err%error(), "Invalid matrix size")
-        if (allocated(error)) return
-
-        return        
-    end subroutine test_error_handling_expm_c    
-    subroutine test_error_handling_expm_z(error)
-        type(error_type), allocatable, intent(out) :: error
-        ! Problem dimension.
-        integer(ilp), parameter :: n = 5, m = 6
-        ! Test matrix.
-        complex(dp) :: A(n, n), E(n, n)
-        type(linalg_state_type) :: err
-        integer(ilp) :: i
-
-        ! Initialize matrix.
-        A = 0.0_dp
-        do i = 1, n-1
-            A(i, i+1) = m*1.0_dp
-        enddo
-
-        ! Compute matrix exponential.
-        call matrix_exp(A, E, order=-1, err=err)
-        ! Check result.
-        call check(error, err%error(), "Negative Pade order")
-        if (allocated(error)) return
-
-        call matrix_exp(A, order=-1, err=err)
-        ! Check result.
-        call check(error, err%error(), "Negative Pade order")
-        if (allocated(error)) return
-
-        ! Compute matrix exponential.
-        call matrix_exp(A, E(:n, :n-1), err=err)
-        ! Check result.
-        call check(error, err%error(), "Invalid matrix size")
-        if (allocated(error)) return
-
-        ! Compute matrix exponential.
-        call matrix_exp(A(:n, :n-1), err=err)
-        ! Check result.
-        call check(error, err%error(), "Invalid matrix size")
-        if (allocated(error)) return
-
-        return        
-    end subroutine test_error_handling_expm_z    
+            ! Compute matrix exponential.
+            call matrix_exp(A(:n, :n-1), err=err)
+            ! Check result.
+            call check(error, err%error(), "Invalid matrix size")
+            if (allocated(error)) return
+        end block
+    end subroutine test_error_handling_expm
 
 end module test_linalg_expm
 
